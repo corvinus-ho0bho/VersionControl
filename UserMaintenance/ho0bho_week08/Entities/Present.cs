06537783a1f5e1375b76f23fd06b38cebd73b3af
Brigitta Nagy <brigitta-bernadett.nagy@stud.uni-corvinus.hu>
2021-11-12 19:04:44 +0100

    Present

---
+++ UserMaintenance/ho0bho_week08/Entities/Present.cs
@@ -0,0 +1,26 @@
+﻿using ho0bho_week08.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ho0bho_week08.Entities
+{
+    public class Present : Toy
+    {
+        public SolidBrush PresentColor { get; private set; }
+
+        public Present(Color color)
+        {
+            PresentColor = new SolidBrush(color);
+        }
+
+        protected override void DrawImage(Graphics g)
+        {
+            g.FillEllipse(PresentColor, 0, 0, Width, Height);
+        }
+    }
+}
+
---
+++ UserMaintenance/ho0bho_week08/Entities/PresentFactory.cs
@@ -0,0 +1,19 @@
+﻿using ho0bho_week08.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ho0bho_week08.Entities
+{
+    public class PresentFactory : IToyFactory
+    {
+        public Color BallColor { get; set; }
+        public Toy CreateNew()
+        {
+            return new Ball(BallColor);
+        }
+    }
+}
