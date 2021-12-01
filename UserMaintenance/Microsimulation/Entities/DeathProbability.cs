168e3da346f56bac0ecb1326597697f23f8ecc51
Brigitta Nagy <brigitta-bernadett.nagy@stud.uni-corvinus.hu>
2021-12-01 20:08:17 +0100

    Entities

---
+++ UserMaintenance/Microsimulation/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsimulation.Entities
+{
+    public class DeathProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double DProbability { get; set; }
+    }
+}
